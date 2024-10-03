0c279529faef7390123fa80284355b45eec54477
Pranshu Saini <ps1442003@outlook.com>
2024-10-03 20:49:46 +0530

    Changes in the Config

--- JavaScipt/MiniProject/scripts/config.js
+++ JavaScipt/MiniProject/scripts/config.js
@@ -4,5 +4,5 @@ export const FRAME_RATE = 70;  // Frame rate for the game
 export const SPEED = 5;        // Speed of the cacti and the floor movement
 export const MIN = 1000;       // Minimum time delay for cactus generation
 export const MAX = 3000;       // Maximum time delay for cactus generation
-export const GRAVITY = 3;      // Gravity for the player
-export const JUMP_STRENGTH = 30; // Jump strength for the player
+export const GRAVITY = 1;      // Gravity for the player
+export const JUMP_STRENGTH = 18.5; // Jump strength for the player
